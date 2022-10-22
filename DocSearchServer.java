df56168ebf040f83b2d0c67c898758745a12ed0a
amccully <amccully2001@gmail.com>
2022-10-21 20:57:17 -0700

    Finished?

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -25,7 +25,7 @@ class FileHelpers {
         return result;
     }
     static String readFile(File f) throws IOException {
-        System.out.println(f.toString());
+        //System.out.println(f.toString());
         return new String(Files.readAllBytes(f.toPath()));
     }
 }
@@ -36,7 +36,27 @@ class Handler implements URLHandler {
       this.files = FileHelpers.getFiles(Paths.get(directory));
     }
     public String handleRequest(URI url) throws IOException {
-      return "Don't know how to handle that path!";
+        if (url.getPath().equals("/")) {
+            return String.format("There are " + files.size() + " files to search");
+        }
+        else if (url.getPath().contains("/search")) {
+            String output = "";
+            int filesFound = 0;
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("q")) {
+                for (File file : files) {
+                    if(FileHelpers.readFile(file).contains(parameters[1])) {
+                        output += String.format("\n%s", file.getPath());
+                        filesFound++;
+                    }
+                }
+            }
+            else {
+                output = "Error with query parameter";
+            }
+            return String.format("There were %d files found:%s", filesFound, output);
+        }
+        return "Don't know how to handle that path!";
     }
 }
 
--- DocSearchTest.java
+++ DocSearchTest.java
@@ -1,11 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class DocSearchTest {
 
     @Test
-    public void testHandleURL() throws IOException {
-        Server.start(3030, new Handler("./technical/"));
+    public void testHandleURL() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI test_handle = new URI("/");
+        System.out.println("handleURL gives: " + h.handleRequest(test_handle));
+    }
+
+    @Test
+    public void testSearch1() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI test_handle = new URI("/search?q=strange");
+        System.out.println("handleURL gives: " + h.handleRequest(test_handle));
     }
 }
